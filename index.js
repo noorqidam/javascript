fa6337c4f78c70604571aec6cc4c5f08776ae73e
noorqidam <noorqidam@gmail.com>
2019-09-01 10:14:22 +0700

    Belajar mengetahui perbedaan Var, Let, dan Const

---
+++ index.js
@@ -0,0 +1,16 @@
+// var message = "Hello Coding"; //Var bisa berlaku dimana saja
+let msg1 = "Hello";
+msg3 = "Berhasil";
+
+const msg2 = "Coding"; //Nilainya sudah tetap (tidak dapat diubah)
+msg3 = "Selamat";
+if(true){
+  let msg1 = "yes"
+  var message = "ini message";
+  msg3 = "Selamat";
+  // var message = 1;
+  // let msg1 = 2; //let hanya berlaku di satu tempat
+  // console.log("testing",msg1);
+}
+// console.log(message);
+console.log(msg3);
